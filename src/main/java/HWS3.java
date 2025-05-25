44dd8b004bfc9d67de1fc4ee98ee2d3cd116b56f
Acer <oxanamaybond2@ukr.net>
2025-05-25 19:31:12 +0300

    HWS3good

--- src/main/java/HWS3.java
+++ src/main/java/HWS3.java
@@ -7,6 +7,32 @@ public class HWS3 {
         for (int i = 0; i < hotelRooms.length; i++) {
             hotelRooms[i] = rand.nextInt(25);
         }
+        boolean unsorted = hotelRooms.length > 1;
+        while (unsorted)  {
+
+            boolean elementsSwitched = false;
+            for (int i = 0; i < hotelRooms.length - 1; i++) {
+                int currentElement = hotelRooms [i];
+                int nextElement = hotelRooms [i + 1];
+
+                if (currentElement > nextElement) {
+                    elementsSwitched = true;
+                    unsorted = true;
+                    hotelRooms[i] = nextElement;
+                    hotelRooms[i + 1] = currentElement;
+                }
+            }
+
+            if (!elementsSwitched) {
+                unsorted = false;
+            }
+
+        }
+
+        for (int i = 0; i < hotelRooms.length-1; i++) {
+            System.out.println(hotelRooms[i]);
+        }
 
     }
+
 }
