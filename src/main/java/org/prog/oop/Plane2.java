81c0f2ebc246b068ae130e0d99f324e37cdaa706
Acer <oxanamaybond2@ukr.net>
2025-06-09 00:21:24 +0300

    PlaneHW

--- src/main/java/org/prog/oop/Plane2.java
+++ src/main/java/org/prog/oop/Plane2.java
@@ -1,42 +1,38 @@
-package org.prog.oop;
+//package org.prog.oop;
 
 import java.io.File;
 import java.util.Random;
 import java.util.UUID;
 
-public class Plane2 {
-    public static void main(String[] args) {
+//public class Plane2 {
+  //  public static void main(String[] args) {
 
 
+        //Plane plane = new Plane();
 
-        Plane bigPlane = new Plane();
-        Plane smallPlane = new Plane();
-
-
-        Plane.flight = UUID.randomUUID().toString();
+       // plane.flight = UUID.randomUUID().toString();
 
 
-        System.out.println(UUID.randomUUID().toString());
+      //  System.out.println(UUID.randomUUID().toString());
 
-        System.out.println("ID" + Plane.flight);
+      //  System.out.println("ID" + plane.flight);
 
-        Random random = new Random();
-        Plane.maxPassengers = random.nextInt(201);
-        for (int i = 0; i < 1; i++) {
+      //  Random random = new Random();
+       // plane.maxPassengers = random.nextInt(201);
+       // for (int i = 0; i < 1; i++) {
 
-                System.out.println(random.nextInt(100, 201));
+                      //  System.out.println(random.nextInt(100, 201));
+          //  }
+          //  plane.realypassengers = random.nextInt(plane.maxPassengers + 1);
 
-            Plane.realypassengers = random.nextInt(Plane.maxPassengers + 1);
-
-            for (int і = 0; i < 1; i++) {
-                System.out.println(random.nextInt(Plane.maxPassengers + 1));
-
-
-            }
-        }
-        System.out.println( "We have" + plane + "with ID " + plane.flight + " which has maximum number of passengers " + plane.maxPassengers + " but there are " + plane.realypassengers + " passwngers");
+           // for (int і = 0; i < 1; i++) {
+              //  System.out.println(random.nextInt(plane.maxPassengers + 1));
 
 
-    }
+           // }
+       // }
 
-}
+
+      //  }
+
+   // }
