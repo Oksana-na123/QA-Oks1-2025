0c12a08a75ef2e99c45f3c90d3ee4636c87dd5b8
Acer <oxanamaybond2@ukr.net>
2025-06-08 21:25:01 +0300

    Plane2HW

--- src/main/java/org/prog/HWcar.java
+++
@@ -1,9 +0,0 @@
-package org.prog;
-
-public class HWcar {
-    Car aliceCar = new Car();
-    Car bobCar = new Car();
-    public String color;
-    public String model;
-
-}
---
+++ src/main/java/org/prog/oop/Plane.java
@@ -0,0 +1,19 @@
+package org.prog.oop;
+
+import java.util.Random;
+
+public class Plane {
+    public static String goodreice;
+
+    public String flight;
+    public int maxPassengers;
+    public String random;
+    public int realypassengers;
+
+
+
+
+
+
+}
+
