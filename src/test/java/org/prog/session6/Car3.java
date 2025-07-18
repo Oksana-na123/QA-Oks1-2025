e06f06482e9d1098636f0c3b1c847541310098a3
Acer <oxanamaybond2@ukr.net>
2025-07-18 19:35:20 +0300

    HWTestNg

---
+++ src/test/java/org/prog/session6/Car3.java
@@ -0,0 +1,18 @@
+package org.prog.session6;
+
+public class Car3 {
+    private String brand;
+    private String color;
+    public Car3(String color, String brand) {
+        this.color = color;
+        this.brand = brand;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Car3{color='" + color + "', model='" + brand + "'}";
+    }
+}
+
+
--- src/test/java/org/prog/session6/HwTest.java
+++ src/test/java/org/prog/session6/HwTest.java
@@ -1,5 +1,40 @@
 package org.prog.session6;
 
-public class HwTest {
+import org.testng.annotations.Test;
 
-}
+import java.util.*;
+
+
+
+public class HwTest {
+    @Test
+
+        public  void testMap() {
+            Map<String, List<Car3>> map = new HashMap<>();
+
+            map.put("Oleg", new ArrayList<>());
+            map.put("Oksana", new ArrayList<>());
+            map.put("Igor", new ArrayList<>());
+            map.put("Dina", new ArrayList<>());
+
+            map.get("Oleg").add(new Car3(randomColor(),"BMW"));
+            map.get("Oksana").add(new Car3(randomColor(), "Mazda"));
+            map.get("Igor").add(new Car3(randomColor(),"Opel"));
+            map.get("Dina").add(new Car3(randomColor(),"Geely"));
+            map.get("Dina").add(new Car3(randomColor(),"Jeep"));
+            System.out.println(map);
+        }
+        public static String randomColor() {
+            Random random = new Random();
+            return switch (random.nextInt(5)) {
+                case 0 -> "red";
+                case 1 -> "green";
+                case 2 -> "blue";
+                case 3 -> "yellow";
+                case 4 -> "pink";
+                default -> "black";
+            };
+        }
+    }
+
+
