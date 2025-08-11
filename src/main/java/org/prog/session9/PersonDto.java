51b916ef6882b6bd4a753c73a05da3061dff74bd
Acer <oxanamaybond2@ukr.net>
2025-08-11 15:04:48 +0300

    ------22,07HW

--- src/main/java/org/prog/session9/PersonDto.java
+++ src/main/java/org/prog/session9/PersonDto.java
@@ -2,11 +2,13 @@ package org.prog.session9;
 
 import lombok.Data;
 import lombok.ToString;import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.ToString;
 import lombok.Data;
 import lombok.ToString;
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @ToString
 public class PersonDto {
--- src/main/java/org/prog/session9/StreetDto.java
+++ src/main/java/org/prog/session9/StreetDto.java
@@ -1,8 +1,9 @@
 package org.prog.session9;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.ToString;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @ToString
 public class StreetDto {
